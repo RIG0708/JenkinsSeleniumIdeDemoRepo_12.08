20aa5de0149a79d21f17ede42e4db0940e5cae60
RIG0708 <rumen_igeorgiev@yahoo.com>
2024-08-12 11:06:58 +0300

    chrome driver options

--- SeleniumIDE/TC01IfUserIsInvalidTryAgainTest1.cs
+++ SeleniumIDE/TC01IfUserIsInvalidTryAgainTest1.cs
@@ -21,9 +21,17 @@ public class TC01IfUserIsInvalidTryAgainTest
     [SetUp]
     public void SetUp()
     {
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.AddArgument("headless");
+        options.AddArgument("no-sandbox");
+        options.AddArgument("disable-div-shm-usage");
+        options.AddArgument("disable-gpu");
+        options.AddArgument("window-siza=1920x1080");
+
+        driver = new ChromeDriver(options);
         js = (IJavaScriptExecutor)driver;
         vars = new Dictionary<string, object>();
+
     }
 
     [TearDown]
